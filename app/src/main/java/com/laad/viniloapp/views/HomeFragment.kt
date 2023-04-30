c8a450bd8dca9d467d063076f7ed3ef6eda4d584
AlexB <a.benitezm@uniandes.edu.co>
2023-04-29 19:10:43 -0500

    Listar Albums Ajustes 2

--- app/src/main/java/com/laad/viniloapp/views/HomeFragment.kt
+++ app/src/main/java/com/laad/viniloapp/views/HomeFragment.kt
@@ -7,13 +7,10 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.laad.viniloapp.R
 import com.laad.viniloapp.databinding.FragmentHomeBinding
-import com.laad.viniloapp.models.Album
 import com.laad.viniloapp.viewmodels.HomeViewModel
-import com.laad.viniloapp.viewmodels.SlideshowViewModel
 
 class HomeFragment : Fragment() {
 
