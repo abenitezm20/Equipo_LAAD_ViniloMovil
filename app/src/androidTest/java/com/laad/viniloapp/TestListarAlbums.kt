5028777a952ae41ec2ddc38c8b83e3dcb704f875
AlexB <a.benitezm@uniandes.edu.co>
2023-04-30 10:13:22 -0500

    Ajuste Expresso

--- app/src/androidTest/java/com/laad/viniloapp/TestListarAlbums.kt
+++ app/src/androidTest/java/com/laad/viniloapp/TestListarAlbums.kt
@@ -8,6 +8,7 @@ import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.DrawerActions
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -25,22 +26,22 @@ public class TestListarAlbums {
     @JvmField
     var mActivityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
 
-    /*    @Test
-        fun coleccionistaTest() {
-            print("Inicio Test")
+    @Test
+    fun coleccionistaTest() {
+        print("Inicio Test")
 
-            Thread.sleep(1000)
-            val collectorBtn =
-                onView(allOf(withId(R.id.collector_home_button), ViewMatchers.isDisplayed()))
-            collectorBtn.perform(click())
+        Thread.sleep(1000)
+        val collectorBtn =
+            onView(allOf(withId(R.id.collector_home_button), ViewMatchers.isDisplayed()))
+        collectorBtn.perform(click())
 
-            val menu = onView(allOf(withId(R.id.drawer_layout)))
-            menu.perform(DrawerActions.open())
-            Thread.sleep(1000)
+        val menu = onView(allOf(withId(R.id.drawer_layout)))
+        menu.perform(DrawerActions.open())
+        Thread.sleep(1000)
 
-            val roleAppText = onView(allOf(withId(R.id.textViewAppRole)))
-            roleAppText.check(ViewAssertions.matches(ViewMatchers.withText(R.string.nav_view_role_collector)))
-        }*/
+        val roleAppText = onView(allOf(withId(R.id.textViewAppRole)))
+        roleAppText.check(ViewAssertions.matches(ViewMatchers.withText(R.string.nav_view_role_collector)))
+    }
 
     @Test
     fun listarAlbumesTest() {
@@ -61,18 +62,6 @@ public class TestListarAlbums {
         listAlbumSelec.perform(click());
 
         //buscando el titulo de un album
-        Log.d("listarAlbumesTest", onView(withId(R.id.textView6)).toString())
-        //onView(withId(R.id.textView6)).check(matches(withText("Buscando América")))
-        onView(allOf(withId(R.id.textView6), withText("rwerwerwrwerwer")))
-        onView(allOf(withId(R.id.textView6), withText("Buscando Ámerica"))).check(matches(withText("Buscando Ámerica")))
-        onView(withText(R.string.menu_sort_length)).check(matches(isDisplayed()));
-
-        //val albumTitleText = onView(allOf(withId(R.id.textView6)))
-        //Log.d("listarAlbumesTest", albumTitleText.toString())
-
-        //onView(allOf(withId(R.id.nav_albums), withText("Buscando América")))
-        //albumTitleText.check(ViewAssertions.matches(ViewMatchers.withText("Buscando América")))
-
-
+        onView(allOf(withId(R.id.textView6), withText("Poeta del pueblo"))).check(matches(isDisplayed()))
     }
 }
