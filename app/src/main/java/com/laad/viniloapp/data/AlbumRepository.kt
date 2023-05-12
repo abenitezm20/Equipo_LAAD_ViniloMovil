0afeebad5621e7f1421b227b3d7b08aaef834484
DanielOchoaSuarez <d.ochoas@uniandes.edu.co>
2023-05-11 20:12:13 -0500

    Ajuste vencimiento cache (#41)

--- app/src/main/java/com/laad/viniloapp/data/AlbumRepository.kt
+++ app/src/main/java/com/laad/viniloapp/data/AlbumRepository.kt
@@ -7,10 +7,17 @@ import android.util.Log
 import com.laad.viniloapp.data.database.CachedAlbumsDao
 import com.laad.viniloapp.data.network.ViniloServiceAdapter
 import com.laad.viniloapp.models.Album
+import com.laad.viniloapp.utilities.CACHE_EXPIRATION_TIME
 
 class AlbumRepository(val application: Application, private val cachedAlbumsDao: CachedAlbumsDao) {
 
+    companion object {
+        var startTime: Long = System.currentTimeMillis()
+    }
+
     suspend fun consultaAlbum(): List<Album> {
+
+        processExpiredCache()
         var cachedAlbums = cachedAlbumsDao.getAlbums()
 
         return if (cachedAlbums.isNullOrEmpty()) {
@@ -33,6 +40,16 @@ class AlbumRepository(val application: Application, private val cachedAlbumsDao:
         val cm =
             application.baseContext.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
         return cm.activeNetworkInfo?.type != ConnectivityManager.TYPE_WIFI && cm.activeNetworkInfo?.type != ConnectivityManager.TYPE_MOBILE
+    }
+
+    private suspend fun processExpiredCache() {
+        Log.d("AlbumRepository", "Tiempo inicio cache $startTime")
+        val endTime = System.currentTimeMillis()
+        if ((endTime - startTime) > CACHE_EXPIRATION_TIME) {
+            Log.d("AlbumRepository", "Venció cache")
+            startTime = endTime
+            cachedAlbumsDao.deleteAll()
+        }
     }
 
 }
