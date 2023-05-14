d80871df33c94b2f0899788efae671403006ff40
AlexB <a.benitezm@uniandes.edu.co>
2023-05-13 20:17:22 -0500

    Ajustes Test

--- app/src/androidTest/java/com/laad/viniloapp/TestListarArtistas.kt
+++ app/src/androidTest/java/com/laad/viniloapp/TestListarArtistas.kt
@@ -70,7 +70,7 @@ public class TestListarArtistas {
         Espresso.onView(
             Matchers.allOf(
                 ViewMatchers.withId(R.id.textView7),
-                ViewMatchers.withText("Nombre: Rubén Blades Bellido de Luna")
+                ViewMatchers.withText("Nombre: Freddie Mercury")
             )
         ).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
     }
